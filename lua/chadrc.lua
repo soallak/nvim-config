9c47eae86937ea8a780c2c27b642421d39e5697e
Saad Ouassil Allak <ouassil@allak.fr>
2025-08-30 21:03:14 +0200

    feat: add more options to chadrc

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,28 +1,46 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "hiberbee",
+  theme = "bearded-arc",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
 -- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
---}
+
+M.ui = {
+  tabufline = {
+    lazyload = false,
+  },
+
+  cmp = {
+    icons = true,
+    lspkind_text = true,
+    style = "flat_light", -- default/flat_light/flat_dark/atom/atom_colored
+  },
+
+  telescope = { style = "bordered" }, -- borderless / bordered
+
+  statusline = {
+    theme = "default", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "arrow",
+    order = nil,
+    modules = nil,
+  },
+}
 
 M.cheatsheet = {
-  theme = "simple", -- simple/grid
+  theme = "grid", -- simple/grid
 }
 
 return M
